d1aab8b59628e59300e57d072b4ce0b7cf198c5b
refirio <refirio@gmail.com>
2020-09-05 19:20:12 +0900

    カテゴリ管理テーブルを作成

--- database/migrations/2020_09_05_191509_create_categories_table.php
+++ database/migrations/2020_09_05_191509_create_categories_table.php
@@ -14,9 +14,14 @@ class CreateCategoriesTable extends Migration
     public function up()
     {
         Schema::create('categories', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
             $table->bigIncrements('id');
+            $table->string('name')->nullable()->comment('名前');
+            $table->integer('sort')->comment('並び順');
             $table->timestamps();
+            $table->softDeletes();
         });
+        DB::statement('ALTER TABLE categories COMMENT \'カテゴリ\'');
     }
 
     /**
